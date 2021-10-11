569c615cc5c7b9608f483ea5b15e57bd477c94a5
havocsupremecy209 <gshukla209@gmail.com>
2021-10-11 19:29:02 -0400

    moved exit with error and exit with success functions to head.h

--- src/head.c
+++ src/head.c
@@ -7,22 +7,6 @@ If a valid flag is given, it'll print x amount of lines/bytes of the file
 
 #include "head.h"
 
-
-/* exits program with success status (0) */
-void
-exit_success()
-{
-    _exit(0);
-}
-
-/* Exits the program with failure */
-void
-exit_bc_failure(char *str)
-{
-    printf("ERROR: %s\n", str);
-    _exit(EXIT_FAILURE);
-}
-
 /* takes whatever is given in stdin and prints it to stdout (the screen)
    Will go for linecount number of times.*/
 void 
@@ -64,6 +48,7 @@ main(int argc, char *argv[])
     printf("entered main function\n");
     /* if countlines=0, countbytes is the argument */
     int countlines = 10;
+    bool flags = false;
 
     /* if no flags or textfile given */
     if (argc == 1)
@@ -74,6 +59,7 @@ main(int argc, char *argv[])
     /* is the first argument a flag?? */
     if (argv[1][0] == '-')
     {
+        flags = true;
         /* check if flag is valid, '-n' or '-c' */
         if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "-n") == 0 )
         {
@@ -111,83 +97,51 @@ main(int argc, char *argv[])
             exit_bc_failure(err_msg);
         }
     }
-    if (argc == 3)
-    printf("countlines is %d\n", countlines);
 
-    // make sure that there's another 2 arguments on top of that, otherwise require argument
-    // otherwise first argument is file, error out if flag doesn't work
+    /* At this point, there either is flags + a file, or just a file */
 
-    // 
+    char filename[100];
 
-    // if both flags given with no file, or file and one flag given
-    /*if (argc == 3)
+    /* If there are flags, -n or -c */
+    if (flags == true)
     {
-        //if only flag and file given, in that order
-        // illegal [line/byte] count
-        if (strlen(argv[1]) == 2 )
-        {
-            // -n flag + some_other_argument
-            if (strcmp(argv[1], NEWLINES_FLAG) == 0)
-            {
-                printf("head: illegal line count -- %s\n", argv[2]);
-            }
-            // -c flag + some_other_argument
-            else if (strcmp(argv[1], COUNTBYTES_FLAG) == 0)
-            {
-                printf("head: illegal byte count -- %s\n", argv[2]);
-            }
-            else
-            {
-                // if its a flag, say its unknown, otherwise
-                // those two flags are probably 
-                if(argv[])
-                else
-                {
-                    printf("head: %s: No such file or directory\n");
-                    incorrect_usage();
-                }
-            }
-        }
-        incorrect_usage();
-    }*/
-
-    if (argc == 2)
-    {    
-        // open file as read only
-        int fd;
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1)
-            exit_bc_failure("file could not be opened!");
-
-        // read file from file descriptor
-        char buffer[MAX_BUFFER_SIZE];
-        if (read(fd, buffer, MAX_BUFFER_SIZE-1) == -1)
-            exit_bc_failure("file could not be read!");
-
-        int currentLine = 1;
-        int i;
-        // start reading file
-        for (i = 0; buffer[i] != '\0'; i++)
-        {
-
-
-            printf("%c", buffer[i]);
-            
-            // if new line, 
-            if (buffer[i] == '\n')
-            {
-                currentLine++;
-                
-                // edgecase with greater/equal than 2 newlines
-                if (buffer[i+1] != '\0')
-                if (buffer[i+1] == '\n')
-                {
-                    buffer[i+1] = 0;
-                }
-                if (currentLine == 11) break;
-            }
-        }
-        if (buffer[i] != '\n') printf("\n");
+        strcpy(filename, argv[3]);
     }
+    else
+    {
+        strcpy(filename, argv[1]);
+    }
+    // open file as read only
+    int fd;
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        exit_bc_failure("file could not be opened!");
+
+    // read file from file descriptor
+    char buffer[MAX_BUFFER_SIZE];
+    if (read(fd, buffer, MAX_BUFFER_SIZE-1) == -1)
+        exit_bc_failure("file could not be read!");
+
+    int currentLine = 1;
+    int i;
+    // start reading file
+    for (i = 0; buffer[i] != '\0'; i++)
+    {
+        printf("%c", buffer[i]);
+        
+        // if new line, 
+        if (buffer[i] == '\n')
+        {
+            currentLine++;
+            
+            // edgecase with greater/equal than 2 newlines
+            if (buffer[i+1] != '\0')
+            {
+                if (buffer[i+1] == '\n') { buffer[i+1] = 0; }
+                if (currentLine == 11) { break;}
+            }
+        }
+    }
+    if (buffer[i] != '\n') printf("\n");
     return 0;
 }
--- src/head.h
+++ src/head.h
@@ -13,4 +13,19 @@
 #define COUNTBYTES_FLAG "-c"
 #define MAX_BUFFER_SIZE 4096
 
+/* exits program with success status (0) */
+void
+exit_success()
+{
+    _exit(0);
+}
+
+/* Exits the program with failure */
+void
+exit_bc_failure(char *str)
+{
+    printf("ERROR: %s\n", str);
+    _exit(EXIT_FAILURE);
+}
+
 #endif
